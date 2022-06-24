cb51833af0c8eb909e7c070a00452b5132b733db
Leonardo Bustamante <leobusta@gmail.com>
2022-06-24 08:32:44 -0500

    cadenas test

--- src/javaapplication1/Cadenas.java
+++ src/javaapplication1/Cadenas.java
@@ -57,7 +57,9 @@ public class Cadenas {
         String sf2 = String.format("value is %f", 32.33434);
         String sf3 = String.format("value is %10.2f", 32.33434);//returns 12 char fractional part
 
-        System.out.println(sf1);
+        
+        
+        System.out.println(sf1); 
         System.out.println(sf2);
         System.out.println(sf3);
     }
